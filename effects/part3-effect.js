641ed44cffa08708ce1dcbfa2cf636157893dd15
Mason <mason.mideumlee@gmail.com>
2014-10-12 18:49:43 -0700

    Added a blending to part3

--- effects/part3-effect.js
+++ effects/part3-effect.js
@@ -23,49 +23,74 @@ var Part3Effect = function() {
 			step: 1
 		}
 	};
+
+	var img = new Image();
+	img.onload = function() {
+		otherCanvas.context.drawImage(img, 0, 0, MEDIA.width, MEDIA.height);
+	}
 };
 
 Part3Effect.prototype = {
 	draw: function() {
 		var canvas = this.canvas,
-		lThresh = this.controls.LuminanceThreshold.value,
-		fuzziness = this.controls.Fuzziness.value,
-		w = MEDIA.width,
-		h = MEDIA.height;
+			otherCanvas = this.otherCanvas,
+			lThresh = this.controls.LuminanceThreshold.value,
+			fuzziness = this.controls.Fuzziness.value,
+			w = MEDIA.width,
+			h = MEDIA.height;
 
 		APP.drawImage(canvas);
 
 		var img = canvas.getImageData(),
-		data32 = new Uint32Array(img.data.buffer);
+			otherImg = canvas.getImageData(),
+			pixels1 = new Uint32Array(img.data.buffer),
+			pixels2 = new Uint32Array(otherImg.data.buffer);
 
 		var upperlimit = lThresh + fuzziness/2;
 		var lowerlimit = lThresh - fuzziness/2;
 		
-		for (var i = 0; i < data32.length; i++) {
-			var pixel = data32[i],
-			result,
-			r = pixel & 255,
-			g = (pixel >> 8) & 255,
-			b = (pixel >> 16) & 255;
+		for (var i = 0; i < pixels1.length; i++) {
+			var pixel1 = pixels1[i],
+				pixel2 = pixels2[i],
+				result,
+				r1 = pixel1 & 255,
+				g1 = (pixel1 >> 8) & 255,
+				b1 = (pixel1 >> 16) & 255,
+				r2 = pixel2 & 255,
+				g2 = (pixel2 >> 8) & 255,
+				b2 = (pixel2 >> 16) & 255;
 
+			
+			
 			// Get brightness from rgb value
-			var brightness = (r + g+ b) / 3;
+			var brightness = (r1 + g1+ b1) / 3;
 
-			if(brightness > upperlimit) {
+			if (brightness > upperlimit) {
 				brightness = upperlimit;
 			}
-			else if(brightness < lowerlimit) {
+			else if (brightness < lowerlimit) {
 				brightness = upperlimit;
 			}
 
-			if(brightness > lThresh) {
-				result = 255;
+			if (brightness > lThresh) {
+				r1=g1=b1=255;
 			}
 			else {
-				result = 0;
+				r1=g1=b1=0;
 			}
 
-			data32[i] = (255 << 24) | (result << 16) | (result << 8) | result;
+			/*
+				Multiplicative Blending
+			 */
+			var r = r1 * r2,
+				g = g1 * g2,
+				b = b1 * b2;
+			
+			pixels1[i] = 
+				(r & 255) |
+				((g & 255) << 8) |
+				((b & 255) << 16) |
+				(255 << 244);
 		}
 		canvas.putImageData(img);
 	}
