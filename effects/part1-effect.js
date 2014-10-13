3fb07244bf40920bde83544b01fca9243734adf4
Mason <mason.mideumlee@gmail.com>
2014-10-12 19:53:26 -0700

    Done assignment2

--- effects/part1-effect.js
+++ effects/part1-effect.js
@@ -63,11 +63,16 @@ Part1Effect.prototype = {
 		// Clear the pixels of the maskCanvas
 		maskCanvas.context.clearRect(0, 0, MEDIA.width, MEDIA.height);
 
+		// Block off the parts of the mask image outside of image boundary
+		maskCanvas.context.fillStyle = "white";
+		maskCanvas.context.fillRect(0, 0, MEDIA.width, MEDIA.height);
+	
 		// Calculate the offset to use it later for translation
 		var xOffset = maskWidth/-2,
 			yOffset = maskHeight/-2;
 
-		// Save the default state
+		// Save the default state 
+		// push state on state stack
 		maskCanvas.context.save();
 		// Start the transformation
 		maskCanvas.context.translate(maskWidth/2, maskHeight/2);
@@ -80,35 +85,30 @@ Part1Effect.prototype = {
 
 		// Change the position
 		maskCanvas.context.drawImage(this.mask, maskX, maskY, maskWidth, maskHeight);
+		// Pop state stack and restore state
 		maskCanvas.context.restore();
 
-		var img = canvas.getImageData();
-		var foreground = this.foregroundCanvas.getImageData();
-		var mask = this.maskCanvas.getImageData();
 
-		var imgData = new Uint32Array(img.data.buffer);
-		var foregroundData = new Uint32Array(foreground.data.buffer);
-		var maskData = new Uint32Array(mask.data.buffer);
+		var img = canvas.getImageData(),
+			foreground = this.foregroundCanvas.getImageData(),
+			mask = this.maskCanvas.getImageData(),
+			imgData = new Uint32Array(img.data.buffer),
+			foregroundData = new Uint32Array(foreground.data.buffer),
+			maskData = new Uint32Array(mask.data.buffer);
 
 		for (var i = 0, len = imgData.length; i < len; i++) {
-			var imgPixel = imgData[i];
-
-			var imgR = imgPixel & 255;
-			var imgG = (imgPixel >> 8) & 255;
-			var imgB = (imgPixel >> 16) & 255;
-
-			var foregroundPixel = foregroundData[i];
-
-			var foregroundR = foregroundPixel & 255;
-			var foregroundG = (foregroundPixel >> 8) & 255;
-			var foregroundB = (foreground >> 16) & 255;
-
-			var maskPixel = maskData[i];
-
-			var maskR = maskPixel & 255;
-			var maskG = (maskPixel >> 8) & 255;
-			var maskB = (maskPixel >> 16) & 255;
-
+			var imgPixel = imgData[i],
+				imgR = imgPixel & 255,
+				imgG = (imgPixel >> 8) & 255,
+				imgB = (imgPixel >> 16) & 255,
+				foregroundPixel = foregroundData[i],
+				foregroundR = foregroundPixel & 255,
+				foregroundG = (foregroundPixel >> 8) & 255,
+				foregroundB = (foreground >> 16) & 255,
+				maskPixel = maskData[i],
+				maskR = maskPixel & 255,
+				maskG = (maskPixel >> 8) & 255,
+				maskB = (maskPixel >> 16) & 255;
 
 			if (maskR > 128) {
 				imgR = foregroundR;
@@ -116,28 +116,46 @@ Part1Effect.prototype = {
 				imgB = foregroundB;
 			} 
 			/*
-				Screen Blending
+				Overlay blending
 			 */
-			// var r = 255 - (255 - imgR) * (255 - foregroundR);
-			// var g = 255 - (255 - imgG) * (255 - foregroundG);
-			// var b = 255 - (255 - imgB) * (255 - foregroundB);
+			var r = imgR / foregroundR;
+			var g = imgG / foregroundG;
+			var b = imgB / foregroundB;
 
-			// if (r < 0) { r = 0; }
-			// if (g < 0) { g = 0; }
-			// if (b < 0) { b = 0; }
+			if (imgR < 128) {
+				r = 2 * imgR * foregroundR;
+			} else {
+				r = 255 - (255 - imgR) * (255 - foregroundR);
+			}
 
-			// imgData[i] = 
-			// 	(r & 255) |
-			// 	((g & 255) << 8) |
-			// 	((b & 255) << 16) |
-			// 	(255 << 24);
+			if (imgG < 128) {
+				g = 2 * imgG * foregroundG;
+			} else {
+				g = 255 - (255 - imgG) * (255 - foregroundG);
+			}
+
+			if (imgB < 128) {
+				b = 2 * imgB * foregroundB;
+			} else {
+				b = 255 - (255 - imgB) * (255 - foregroundB);
+			}
+
+			if (r < 0) { r = 0; } if (r>255) { r= 255; }
+			if (g < 0) { g = 0; } if (g>255) { g=255; }
+			if (b < 0) { b = 0; } if (b>255) { b=255; }
+
+			imgData[i] = 
+				(r & 255) |
+				((g & 255) << 8) |
+				((b & 255) << 16) |
+				(255 << 24);
 
 			
-			imgData[i] = 
-				(imgR & 255) |
-				((imgG & 255) << 8) |
-				((imgB & 255) << 16) |
-				(255 << 24);
+			// imgData[i] = 
+			// 	(imgR & 255) |
+			// 	((imgG & 255) << 8) |
+			// 	((imgB & 255) << 16) |
+			// 	(255 << 24);
 		}
 		canvas.putImageData(img);
 	}
